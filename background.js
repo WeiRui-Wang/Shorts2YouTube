7bfb969aaa410eeb6e883e868ba2d61545c236d9
WeiRui Wang <wnwang@ncsu.edu>
2023-02-25 21:38:01 -0500

    Added icon and implemented adaptive status display indicator #1

---
+++ background.js
@@ -0,0 +1,18 @@
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.action.disable();
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+        let rule = {
+            conditions: [
+                new chrome.declarativeContent.PageStateMatcher({
+                    pageUrl: {
+                        hostSuffix: '.youtube.com',
+                        urlContains: 'youtube.com/shorts/'
+                    },
+                })
+            ],
+            actions: [new chrome.declarativeContent.ShowAction()],
+        };
+        let rules = [rule];
+        chrome.declarativeContent.onPageChanged.addRules(rules);
+    });
+});
